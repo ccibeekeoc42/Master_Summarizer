1dc4616c4c64001fac6506e64ee2bab69884cf2d
ccibeekeoc42 <ccibeekeoc42@gmail.com>
2023-04-15 00:00:43 -0700

    Simplified the chat feature

--- app.py
+++ app.py
@@ -1,15 +1,9 @@
 import streamlit as st
-import PyPDF2
 import os
-from langchain.chains import ConversationChain
-from langchain.chains.conversation.memory import ConversationEntityMemory
-from langchain.chains.conversation.prompt import ENTITY_MEMORY_CONVERSATION_TEMPLATE
-from langchain import OpenAI
-from streamlit_chat import message
-from summarizer import summarize_pdf
 from youtube_page import render_youtube_page
 from wikipedia_page import render_wikipedia_page
 from pdf_page import render_pdf_page
+from chat_page import render_chat_page
 
 
 st.set_page_config(page_title="PDF Reader and YouTube Player",
@@ -22,57 +16,10 @@ os.environ['OPENAI_API_KEY'] = api_key
 tab_names = ["YouTube", "Wikipedia", "PDF", "Chat"]
 tab_choice = st.sidebar.radio("Choose a Tab", tab_names)
 
-uploaded_files = {}
-
 if api_key:
-    if tab_choice == "YouTube":
-        render_youtube_page(api_key)
-    elif tab_choice == "Wikipedia":
-        render_wikipedia_page(api_key)
-    elif tab_choice == "PDF":
-        render_pdf_page(api_key)
-    elif tab_choice == "Chat":
-        st.subheader("Chat💬")
-        # Storing Sessions
-        if 'generated' not in st.session_state:
-            st.session_state['generated'] = []  # output
-        if 'past' not in st.session_state:
-            st.session_state['past'] = []  # user input
-        if 'input' not in st.session_state:
-            st.session_state['input'] = ""
-        if "stored_session" not in st.session_state:
-            st.session_state["stored_session"] = []
-
-        def get_text():
-            """Gets the user input and returns the string entered by the user"""
-            input_text = st.text_input("You: ", st.session_state['input'], key='input',
-                                       placeholder="Yor AI assistant here! Ask anything...",
-                                       label_visibility='hidden')
-            return input_text
-
-        llm = OpenAI(openai_api_key=api_key, temperature=0.5,
-                     model_name='gpt-3.5-turbo')
-        # create conv memory
-        if 'entity_memory' not in st.session_state:
-            st.session_state['entity_memory'] = ConversationEntityMemory(
-                llm=llm, k=10)
-        # Create conversation chain
-        conversation = ConversationChain(
-            llm=llm,
-            prompt=ENTITY_MEMORY_CONVERSATION_TEMPLATE,
-            memory=st.session_state['entity_memory'])
-        user_input = get_text()
-        if user_input:
-            output = conversation.run(input=user_input)
-            st.session_state.past.append(user_input)
-            st.session_state.generated.append(output)
-
-        with st.expander("Thread", expanded=True):
-            for i in reversed(range(len(st.session_state.generated))):
-                message(st.session_state["generated"][i], key=str(i))
-                message(st.session_state['past'][i],
-                        is_user=True, key=str(i)+'_user')
-    else:
-        st.write("Bad File")
-else:
-    st.error("No Secret Key Found!!")
+    if tab_choice == "YouTube": render_youtube_page(api_key)
+    elif tab_choice == "Wikipedia": render_wikipedia_page(api_key)
+    elif tab_choice == "PDF": render_pdf_page(api_key)
+    elif tab_choice == "Chat": render_chat_page(api_key)
+    else: st.write("Bad File")
+else: st.error("No Secret Key Found!!")
---
+++ chat_page.py
@@ -0,0 +1,45 @@
+from streamlit_chat import message
+import streamlit as st
+from speech_eng import text2voice
+
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import HumanMessage, SystemMessage, AIMessage
+
+
+def render_chat_page(api_key):
+    """Renders the chat page"""
+    st.subheader("Chat💬")
+    # Storing Sessions
+    if 'generated' not in st.session_state:
+        st.session_state['generated'] = []  # output
+    if 'past' not in st.session_state:
+        st.session_state['past'] = []  # user input
+    if 'input' not in st.session_state:
+        st.session_state['input'] = ""
+    if "chat_messages" not in st.session_state:
+        st.session_state["chat_messages"] = [SystemMessage(content="You are an intelligent and helpful assistant, called Chris AI"),]
+
+    def get_text():
+        """Gets the user input and returns the string entered by the user"""
+        input_text = st.text_input("You: ", st.session_state['input'], key='input',
+                                placeholder="Yor AI assistant here! Ask anything...",
+                                label_visibility='hidden')
+        return input_text
+    
+    chat = ChatOpenAI(openai_api_key=api_key, model_name="gpt-3.5-turbo", temperature=0.8)
+    user_input = get_text()
+    output = ""
+    if user_input:
+        st.session_state.chat_messages.append(HumanMessage(content=user_input))
+        output = chat(messages=st.session_state.chat_messages)
+        st.session_state.past.append(user_input)
+        st.session_state.generated.append(output.content)
+        st.session_state.chat_messages.append(AIMessage(content=output.content))
+
+    with st.expander("Thread", expanded=True):
+        for i in reversed(range(len(st.session_state.generated))):
+            message(st.session_state["generated"][i], key=str(i))
+            message(st.session_state['past'][i],
+                    is_user=True, key=str(i)+'_user')
+    if output: 
+        st.audio(text2voice(str(output.content)), format="audio/wav")
